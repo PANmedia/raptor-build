2a58fd73d72bc25f810ade1d50e73d090d633793
David Neilsen <david@panmedia.co.nz>
2015-01-27 09:31:26 +1300

    Update builder/clean CSS

--- build/raptor-builder.js
+++ build/raptor-builder.js
@@ -1,5 +1,5 @@
 var _ = require('lodash');
-var cleanCSS = require('clean-css');
+var CleanCSS = require('clean-css');
 var fs = require('fs');
 var fs2 = require('file');
 var path = require('path');
@@ -126,10 +126,6 @@ function outputModuleNames() {
 };
 
 function addPath(path) {
-    if (!fs.existsSync(path)) {
-        lib.writeLine('Skipping missing path: ' + path);
-        return;
-    }
     lib.writeDebug('Adding path: ' + fs.realpathSync(root + path));
     paths.push(fs.realpathSync(root + path));
 };
@@ -195,11 +191,48 @@ function getModules() {
 
         addedModules = added;
 
-        lib.writeDebug('Added modules: ' + addedLog.join(', '));
-        lib.writeDebug('Skipped modules: ' + skippedLog.join(', '));
-        lib.writeDebug('Excluded modules: ' + excludedLog.join(', '));
+        lib.writeDebug('Added modules: \n    ' + addedLog.alphanumSort().join(', \n    ') + '\n');
+        lib.writeDebug('Skipped modules: \n    ' + skippedLog.alphanumSort().join(', \n    ') + '\n');
+        lib.writeDebug('Excluded modules: \n    ' + excludedLog.alphanumSort().join(', \n    ') + '\n');
     }
     return addedModules;
+};
+
+Array.prototype.alphanumSort = function(caseInsensitive) {
+    caseInsensitive = true;
+  for (var z = 0, t; t = this[z]; z++) {
+    this[z] = [];
+    var x = 0, y = -1, n = 0, i, j;
+
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
+      if (m !== n) {
+        this[z][++y] = "";
+        n = m;
+      }
+      this[z][y] += j;
+    }
+  }
+
+  this.sort(function(a, b) {
+    for (var x = 0, aa, bb; (aa = a[x]) && (bb = b[x]); x++) {
+      if (caseInsensitive) {
+        aa = aa.toLowerCase();
+        bb = bb.toLowerCase();
+      }
+      if (aa !== bb) {
+        var c = Number(aa), d = Number(bb);
+        if (c == aa && d == bb) {
+          return c - d;
+        } else return (aa > bb) ? 1 : -1;
+      }
+    }
+    return a.length - b.length;
+  });
+
+  for (var z = 0; z < this.length; z++)
+    this[z] = this[z].join("");
+  return this;
 };
 
 function build() {
@@ -426,7 +459,7 @@ function compressJS() {
 function compressCSS(inputFile, outputFile) {
     lib.writeDebug('Cleaning CSS: ' + inputFile);
     var css = fs.readFileSync(inputFile).toString();
-    var cssMin = cleanCSS.process(css);
+    var cssMin = new CleanCSS(options).minify(css).styles;
     fs.writeFileSync(outputFile, cssMin);
     lib.statFile(outputFile);
     return cssMin;
